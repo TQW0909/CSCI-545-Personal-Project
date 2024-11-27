b80e3f1f07585b4db90a8750c721f503419a6e77
Tingqi Wang <twang356@usc.edu>
2024-11-26 18:34:57 -0800

    Initial code for YOLO

---
+++ yolo_object_detection.py
@@ -0,0 +1,33 @@
+from ultralytics import YOLO
+import cv2
+import os
+
+images_dir = "images"
+
+if not os.path.exists(images_dir):
+    print(f"Error: Directory '{images_dir}' does not exist.")
+    exit(1)
+
+# Load images using OpenCV
+images = []
+for image in os.listdir(images_dir):
+    img_path = os.path.join(images_dir, image)
+    img = cv2.imread(img_path)
+    if img is not None:
+        images.append(img)
+
+
+# Load the YOLOv11s model
+model = YOLO('yolov11s.pt')
+
+results = model(images)
+
+# Process results list
+for result in results:
+    boxes = result.boxes  # Boxes object for bounding box outputs
+    masks = result.masks  # Masks object for segmentation masks outputs
+    # keypoints = result.keypoints  # Keypoints object for pose outputs
+    probs = result.probs  # Probs object for classification outputs
+    obb = result.obb  # Oriented boxes object for OBB outputs
+    result.show()  # display to screen
+    result.save(filename="result.jpg")  # save to disk
